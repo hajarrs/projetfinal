f9b8e67cbd5b4a96fd3c78208cfb8d5fb8149569
hajar <qaibouhajar@gmail.com>
2021-03-03 12:14:30 +0100

    commit MAJ

--- etablissementAngular/src/app/service/professeur.service.ts
+++ etablissementAngular/src/app/service/professeur.service.ts
@@ -13,40 +13,40 @@ export class ProfesseurService {
 
   private httpHeaders: HttpHeaders;
 
-   constructor(private http: HttpClient) { 
+   constructor(private http: HttpClient) {
     this.httpHeaders = new HttpHeaders({
       'content-type': 'application/json',
-      Authorization: 'Basic ' + btoa('${login.login}:${login.password}'),
+      Authorization: 'Basic ' + sessionStorage.getItem('tokenId'),
     });
   }
 
     public allProfesseur(): Observable<Professeur[]> {
       return this.http.get<Professeur[]>(this.url, { headers: this.httpHeaders });
     }
-  
+
     public delete(id: number): Observable<void> {
       return this.http.delete<void>(this.url + '/' + id, {
         headers: this.httpHeaders,
       });
     }
-  
+
     public update(professeur: Professeur): Observable<Professeur> {
       return this.http.put<Professeur>(`${this.url}/${professeur.id}`, professeur, {
         headers: this.httpHeaders,
       });
     }
-  
+
     public insert(professeur: Professeur): Observable<Professeur> {
       const o = {
-        //id: professeur.id,  
-        login: professeur.login, 
+        //id: professeur.id,
+        login: professeur.login,
         nom: professeur.nom,
         prenom: professeur.prenom,
         adresse: professeur.adresse,
         dateNaissance: professeur.dateNaissance,
         etablissement: professeur.etablissement,
 
-        
+
       };
       return this.http.post<Professeur>(this.url+'/add', o, { headers: this.httpHeaders });
     }
